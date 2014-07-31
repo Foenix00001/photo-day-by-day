3a8bf89915f79f15ecb84251df5a4c73773268f7
Foenix <foenix.i@gmail.com>
2014-07-31 22:17:37 +0400

    Initial commit

---
+++ src/pro/foenix/photodaybyday/YearActivity.java
@@ -0,0 +1,15 @@
+package pro.foenix.photodaybyday;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class YearActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_year);
+    }
+}
